d6386186219052a819bbe36efcabad7da6779eae
Akintola Emmanuel <emmanuelakintola007@gmail.com>
2025-01-01 02:53:50 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function open() {
+    if (document.getElementById('content').style.display="block") {
+        document.getElementById('content').style.display="none";
+    } else {
+        document.getElementById('content').style.display="block";
+    }
+    
+}
